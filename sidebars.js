4b72f41855e6e0e32d09e490d9fc6e6e871e53fa
JimmyWongHJW <55980257+JimmyWongHJW@users.noreply.github.com>
2023-12-31 16:06:39 +0800

    1231

--- sidebars.js
+++ sidebars.js
@@ -1,19 +1,10 @@
 module.exports = {
   tutorialSidebar: [
-    'intro',
     {
       type: 'category',
-      label: '🍎Apple ID',
-      link: {
-        type: 'generated-index',
-		  title: '🍎Apple ID 商品说明',
-        keywords: ['tutorial-appleid'],
-      },
-	  items: [
-        'tutorial-appleid/appleid',
-        'tutorial-appleid/tutorial',
-        'tutorial-appleid/question',
-      ],
-	},  
+      label: 'Tutorial',
+      items: ['intro'],
+    },
   ],
-};	     
+   
+};
