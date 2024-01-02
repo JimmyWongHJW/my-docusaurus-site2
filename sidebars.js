c61146cf146d82664353105f598e3c1674c753a9
JimmyWongHJW <55980257+JimmyWongHJW@users.noreply.github.com>
2024-01-02 21:38:55 +0800

    0102-2138

--- sidebars.js
+++ sidebars.js
@@ -4,6 +4,11 @@ module.exports = {
     {
       type: 'category',
       label: '【多多有货】网站服务',
+	  link: {
+        type: 'generated-index',
+		title: '网站操作指引',
+		keywords: ['tutorial-basics'],
+      },
       items: ['tutorial-basics/mall-intro/purchase-order'],
     },
   ],
